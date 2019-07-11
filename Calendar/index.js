7ff973f0339c3e252620f5f8a41df537650d9919
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-07-11 09:41:40 +0300

    Change Year and Month

--- Calendar/index.js
+++ Calendar/index.js
@@ -8,19 +8,31 @@ let thisDay = [".one", ".two", ".tree", ".four", ".five", ".six", ".seven", ".ei
 ".nineteen",".twenty", ".twenty-one", ".twenty-two", ".twenty-tree", ".twenty-four", ".twenty-five", ".twenty-six", ".twenty-seven", ".twenty-eight", ".twenty-nine", ".thirty", ".thirty-one", ".thirty-two", ".thirty-tree", ".thirty-four", ".thirty-five"]
 const now = new Date();
 const Months = monthNames[now.getMonth()]
-const Years = now.getFullYear()
+let Years = now.getFullYear()
 const MonthNumber = now.getMonth()
 const firstDay = new Date(Years, MonthNumber, 1).getDay()
-
+let dayInMonth = thisMonth((Number(now.getMonth()) + 1), now.getFullYear())
+let OneButton = document.querySelector(".oneButton")
+let MonthDate = now.getMonth()
 function thisMonth (month, year) {
   return new Date(year, month, 0).getDate()
 }
+function one() {
+  MonthDate = MonthDate - 1
+  month.innerHTML = monthNames[MonthDate] 
+  if (MonthDate === 0) {
+    MonthDate = 11
+    Years = Years - 1
+    Year.innerHTML = Years 
+  }
+}
+
 
 month.innerHTML = Months
 Year.innerHTML = Years
 
 
-const dayInMonth = thisMonth((Number(now.getMonth()) + 1), now.getFullYear())
+
 for(let i=0; i < dayInMonth; i++) {
   let Monthdays = document.querySelector(thisDay[i + firstDay])
   Monthdays.innerHTML = i + 1;
