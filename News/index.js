2ca37a746dcca151f24f88aa70cf83029767b725
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-07-15 09:51:27 +0300

    New style

---
+++ News/index.js
@@ -0,0 +1,23 @@
+//fetch("https://dev.to/api/articles")
+//.then(data => data.json())
+//.then(data => {
+    //data.forEach(el => {
+        //const titleDiv = document.createElement("div");
+        //titleDiv.innerHTML = el.title;
+        //document.body.appendChild(titleDiv)
+    //})
+//})
+// console.log("A")
+
+// setTimeout(function() {
+//     console.log("C")
+// }, 0)
+
+// setTimeout(function() {
+//     console.log("G")
+// }, 0)
+
+
+// console.log("B")
+fetch("https://dev.to/api/articles ")
+.then(text => )
