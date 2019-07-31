5b60850378183783ee0a85d8e4fbc434621ad179
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-07-31 16:51:07 +0300

    take user text

--- News/page.js
+++ News/page.js
@@ -9,6 +9,7 @@ function home() {
 fetch('https://dev.to/api/articles/' + id).then(data => data.json()).then(data => {
     const user = data.user
     const colums = document.querySelector(".col-sm")
+    const tagList = data.tags
 
     const imgFluid = document.createElement("img")
     imgFluid.src = data.cover_image
@@ -21,16 +22,34 @@ fetch('https://dev.to/api/articles/' + id).then(data => data.json()).then(data =
     title.innerHTML = data.title
     colums.appendChild(title)
 
+    const profileImage = document.createElement("img")
+    profileImage.src = user.profile_image
+    profileImage.className = "profileImage"
+    colums.appendChild(profileImage)
+
+    const userName = document.createElement("p")
+    userName.innerHTML = user.name 
+    userName.className = "userName"
+    colums.appendChild(userName)
+
+    const h2 = document.createElement("h2")
+    for(let i=0; i<5; i++){
+     if(tagList[i] === undefined){
+        tagList[i] = " "
+                     
+     } else {
+              tagList[i] =  " #" + tagList[i]
+         }
+       }
+     h2.innerHTML = tagList[0] + tagList[1] + tagList[2] + tagList[3] + tagList[4]
+     h2.onclick = function tegs() {
+           window.location.href = "./teg.html?" + tagList[0]
+       }
+    colums.appendChild(h2)
+
     const bodyhtml = data.body_html
     const text = document.createElement("p")
     text.innerHTML += bodyhtml
     colums.appendChild(text)
 
-    const profileImage = document.createElement("img")
-    profileImage.src = user.profile_image
-    profileImage.className = "img-fluid"
-    profileImage.alt = "Responsive image"
-    profileImage.id  = "profileImage"
-    colums.appendChild(profileImage)
 })
-
