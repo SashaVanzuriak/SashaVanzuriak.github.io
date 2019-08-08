27341e63f7b038b8bd45b752d78e861f63f8a88f
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-08 09:40:55 +0300

    take div in user.html

--- News/user.js
+++ News/user.js
@@ -1,1 +1,10 @@
-const 
+const data = window.location.href;
+const datas = data.split("#")[1];
+console.log(datas);
+fetch("https://dev.to/api/users/by_username?url=" + datas)
+  .then(element => element.json())
+  .then(element => {
+    body = document.createElement("div");
+    body.className = "body";
+    document.body.appendChild(body);
+  });
