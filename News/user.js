0c8a5a048171203b4f019130eaaf52ff66f391e9
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-09 16:36:32 +0300

    take new img-user

--- News/user.js
+++ News/user.js
@@ -7,4 +7,9 @@ fetch("https://dev.to/api/users/by_username?url=" + datas)
     body = document.createElement("div");
     body.className = "body";
     document.body.appendChild(body);
+
+    image = document.createElement("img");
+    image.className = "img";
+    image.src = element.profile_image;
+    body.appendChild(image);
   });
