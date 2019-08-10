79da1e5208dfc0eba5f36c9ffebdda8876c392c4
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-10 09:23:49 +0300

    take photo

--- News/user.js
+++ News/user.js
@@ -5,11 +5,16 @@ fetch("https://dev.to/api/users/by_username?url=" + datas)
   .then(element => element.json())
   .then(element => {
     body = document.createElement("div");
-    body.className = "body";
+    body.className = "card";
     document.body.appendChild(body);
 
     image = document.createElement("img");
     image.className = "img";
     image.src = element.profile_image;
     body.appendChild(image);
+
+    userName = document.createElement("p");
+    userName.className = "userName";
+    userName.innerHTML = element.name;
+    body.appendChild(userName);
   });
