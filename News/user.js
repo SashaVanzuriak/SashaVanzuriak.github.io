1f0963cb6a3df84ed23b3df69c1f7f41fb623b6c
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-10 09:52:03 +0300

    take text user

--- News/user.js
+++ News/user.js
@@ -4,17 +4,22 @@ console.log(datas);
 fetch("https://dev.to/api/users/by_username?url=" + datas)
   .then(element => element.json())
   .then(element => {
-    body = document.createElement("div");
-    body.className = "card";
-    document.body.appendChild(body);
+    card = document.createElement("div");
+    card.className = "card";
+    document.body.appendChild(card);
 
     image = document.createElement("img");
     image.className = "img";
     image.src = element.profile_image;
-    body.appendChild(image);
+    card.appendChild(image);
 
     userName = document.createElement("p");
     userName.className = "userName";
     userName.innerHTML = element.name;
-    body.appendChild(userName);
+    card.appendChild(userName);
+
+    summary = document.createElement("p");
+    summary.innerHTML = element.summary;
+    summary.className = "summery";
+    card.appendChild(summary);
   });
