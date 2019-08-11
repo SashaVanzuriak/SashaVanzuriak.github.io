4cb83c81f12fec8d5303cae5386e6f48332497ec
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-11 19:04:56 +0300

    fixs mistakse

--- News/user.js
+++ News/user.js
@@ -4,22 +4,18 @@ console.log(datas);
 fetch("https://dev.to/api/users/by_username?url=" + datas)
   .then(element => element.json())
   .then(element => {
-    card = document.createElement("div");
-    card.className = "card";
-    document.body.appendChild(card);
-
     image = document.createElement("img");
     image.className = "img";
     image.src = element.profile_image;
-    card.appendChild(image);
+    document.body.appendChild(image);
 
     userName = document.createElement("p");
     userName.className = "userName";
     userName.innerHTML = element.name;
-    card.appendChild(userName);
+    document.body.appendChild(userName);
 
     summary = document.createElement("p");
     summary.innerHTML = element.summary;
     summary.className = "summery";
-    card.appendChild(summary);
+    document.body.appendChild(summary);
   });
