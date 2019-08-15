39c063f3a45450518fbcb7927dd5d50a7841cec8
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-15 13:32:02 +0300

    Take onclick create()

--- To do list/To do list/toDoList.js
+++ To do list/To do list/toDoList.js
@@ -1,4 +1,13 @@
 const box = document.querySelector(".box");
+const Cheack = document.querySelector(".Todo");
+
+function create() {
+  const Imges = document.createElement("img");
+  Imges.src = "./Img/NonClick.png";
+  Cheack.appendChild(Imges);
+  console.log(1);
+}
+
 function Box() {
   box.src = "./Img/Click.png";
 }
