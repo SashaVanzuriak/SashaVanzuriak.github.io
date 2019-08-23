7c27493f8c1065dcc19179b7d4449f43a4cc02b7
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-23 08:15:33 +0300

    take day

--- To do list/index.js
+++ To do list/index.js
@@ -1,6 +1,7 @@
 const ThisDate = document.querySelector(".date");
+const DayinMonth = document.querySelector(".DayinMonth");
 const now = new Date();
 
-const Days = [
-  "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
-],
+const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+DayinMonth.innerHTML = Days[now.getDay()];
