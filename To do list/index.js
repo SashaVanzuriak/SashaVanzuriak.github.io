12bf349ebdf729ba0b3534aab70d79126a267621
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-25 17:39:56 +0300

    take Month name

--- To do list/index.js
+++ To do list/index.js
@@ -1,5 +1,20 @@
 const now = new Date();
 const today = document.querySelector(".h3");
 const DayWeeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const Months = [
+  "Dec",
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
 
 today.innerHTML += " " + now.getDate() + " " + DayWeeks[now.getDay()];
