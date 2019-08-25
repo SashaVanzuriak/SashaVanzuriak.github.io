8dbcd2b22dd214d68e1afb9bd8b67b9ca2edca48
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-25 10:53:23 +0300

    delete

--- To do list/index.js
+++
@@ -1,7 +0,0 @@
-const ThisDate = document.querySelector(".date");
-const DayinMonth = document.querySelector(".DayinMonth");
-const now = new Date();
-
-const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-DayinMonth.innerHTML = Days[now.getDay()] + ", " + now.getDate();
