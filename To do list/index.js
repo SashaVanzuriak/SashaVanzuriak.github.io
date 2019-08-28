e356da4e02096dd2969a6a0eb3a50d660203a685
SashaVanzuriak <sashavanzuriak@gmail.com>
2019-08-28 12:03:27 +0300

    take delete the plus and text

--- To do list/index.js
+++ To do list/index.js
@@ -1,5 +1,7 @@
 const now = new Date();
 const today = document.querySelector(".h3");
+const plus = document.querySelector(".plus");
+const text = document.querySelector(".add");
 const DayWeeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const Months = [
   "Dec",
@@ -25,3 +27,8 @@ today.innerHTML +=
   now.getDate() +
   " " +
   Months[now.getMonth() + 1];
+
+plus.onclick = function Add() {
+  plus.remove();
+  text.remove();
+};
