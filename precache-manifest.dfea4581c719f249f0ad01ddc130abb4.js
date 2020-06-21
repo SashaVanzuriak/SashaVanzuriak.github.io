4054aec68670a273f37774ce7b5d4b36e363adb9
Sasha <sashavanzuriak@gmail.com>
2020-06-21 13:41:07 +0300

    Updates

---
+++ precache-manifest.dfea4581c719f249f0ad01ddc130abb4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28622b744058cec300a49c2c374c993d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "172bd796a14c5ed037c4",
+    "url": "/static/css/main.865cf567.chunk.css"
+  },
+  {
+    "revision": "7f08b7c5ba3d82ca014f",
+    "url": "/static/js/2.2c858fb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c858fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172bd796a14c5ed037c4",
+    "url": "/static/js/main.a152a40e.chunk.js"
+  },
+  {
+    "revision": "f02a146d10c9565a1a8b",
+    "url": "/static/js/runtime-main.468c329b.js"
+  },
+  {
+    "revision": "8f951314ab42767e13dab41d39750ad6",
+    "url": "/static/media/GetLucky.8f951314.mp3"
+  },
+  {
+    "revision": "97be4414ac20803f145441979158e581",
+    "url": "/static/media/GetLucky.97be4414.jpg"
+  },
+  {
+    "revision": "8b5ed0cd0caff46d520fc00a91efbee2",
+    "url": "/static/media/HarderBetterStronger.8b5ed0cd.jpg"
+  },
+  {
+    "revision": "dbe134b716b4e723d7f0ee3d8891df0b",
+    "url": "/static/media/HarderBetterStronger.dbe134b7.mp3"
+  },
+  {
+    "revision": "581ae5685b2df2d1946792fe2c6c5e52",
+    "url": "/static/media/InstantCrush.581ae568.jpeg"
+  },
+  {
+    "revision": "7763a2e596036170be6dc563e6a362d6",
+    "url": "/static/media/InstantCrush.7763a2e5.mp3"
+  },
+  {
+    "revision": "52bcc2e1286ea354203b98df6046cbee",
+    "url": "/static/media/OneMoreTime.52bcc2e1.jpg"
+  },
+  {
+    "revision": "cfd7a6392a97c45027b313990f192402",
+    "url": "/static/media/OneMoreTime.cfd7a639.mp3"
+  }
+]);
